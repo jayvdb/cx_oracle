3ceb0ce4c559989c7db13f5844ff7be43ba8bb71
John Vandenberg <jayvdb@gmail.com>
2016-05-05 00:31:15 +0700

    Do None first

--- test_external_connect.py
+++ test_external_connect.py
@@ -1,9 +1,9 @@
 import cx_Oracle
 
+connection = cx_Oracle.connect(None, None, "/")
+
+print('connected with None')
+
 connection = cx_Oracle.connect("", "", "/")
 
 print('connected')
-
-connection = cx_Oracle.connect(None, None, "/")
-
-print('connected 2')
