78f2b819b19d65e3c0980f942a92d46cba5c586c
John Vandenberg <jayvdb@gmail.com>
2016-05-05 10:16:43 +0700

    More urls and show connect_args

--- test_external_connect_sqlalchemy.py
+++ test_external_connect_sqlalchemy.py
@@ -1,16 +1,29 @@
-from sqlalchemy.engine import create_engine
+from sqlalchemy.engine import create_engine, url
 
 urls = [
     'oracle:///',
     'oracle:///@XE',
+    'oracle:///:@XE',
+    'oracle:///:@localhost/XE',
+    'oracle://:@localhost/XE',
     'oracle://XE',
+    'oracle://:@XE',
+    'oracle://:@/',
     'oracle:///@localhost/XE',
 ]
 
-for url in urls:
+for connect_url in urls:
+    print('---')
     try:
-        engine = create_engine(url)
-        connection = engine.connect()
-        print('connected using %s' % url)
+        engine = create_engine(connect_url)
+        u = url.make_url(connect_url)
+        print('\n\nurl = %s' % connect_url)
+        print('args = %r \nkwargs = %r' % engine.dialect.create_connect_args(u))
     except Exception as e:
-        print('\nconnection as %s failed: %s' % (url, e))
+        print('\nconnection args for %s failed: %s' % (connect_url, e))
+    try:
+        connection = engine.connect()
+        print('connected using %s' % connect_url)
+    except Exception as e:
+        print('\nconnection as %s failed: %s' % (connect_url, e))
+    print('---')
